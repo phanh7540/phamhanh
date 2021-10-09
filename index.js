050b90e46c03b1729fec280189c4ee590c3387ba
phanh7540 <phanh7540@gmail.com>
2021-10-09 16:16:26 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,89 @@
+const PORT = 3000;		
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+var ip = require('ip');
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+
+var mangaUsers=[];
+var mangbUsers=[];
+
+io.on("connection", function(socket){
+    console.log("Co nguoi ket noi " + socket.id);// hiển thi id kết nối 
+
+    socket.on("client-send-aUsername", function(data){
+     console.log(data) ; // nhận thông tin của tên khách hàng 
+      if(mangaUsers.indexOf(data)>=0){
+        socket.emit("server-send-dki-thatbai");// gửi thông báo đăng ký thất bại về người dùng 
+      }else{
+        mangaUsers.push(data);// đưa data vào mảng mangUsers
+        socket.Usernamea = data;
+        io.sockets.emit("server-send-danhsach-Usersa", mangaUsers);// gửi thông báo đăng ký thành công về người dùng 
+    }
+
+    });
+    socket.on("client-send-bUsername", function(data){
+      console.log(data) ; // nhận thông tin biển số khách hàng
+        if(mangbUsers.indexOf(data)>=0){
+          socket.emit("server-send-dki-thatbai");// gửi thông báo đăng ký thất bại về người dùng 
+        }else{
+          mangbUsers.push(data);// đưa data vào mảng mangUsers
+          socket.Usernameb = data;
+          socket.emit("server-send-dki-thanhcong", data);
+          io.sockets.emit("server-send-danhsach-Usersb", mangbUsers);// gửi thông báo đăng ký thành công về người dùng 
+      }
+    });
+ 
+
+
+});
+
+/*
+var mangUsers=[];
+
+io.on("connection", function(socket){
+  console.log("Co nguoi ket noi " + socket.id);
+
+  socket.on("client-send-Username", function(data){
+    if(mangUsers.indexOf(data)>=0){
+      socket.emit("server-send-dki-thatbai");
+    }else{
+      mangUsers.push(data);
+      socket.Username = data;
+      socket.emit("server-send-dki-thanhcong", data);
+      io.sockets.emit("server-send-danhsach-Users", mangUsers);
+    }
+  });
+
+  socket.on("logout", function(){
+    mangUsers.splice(
+      mangUsers.indexOf(socket.Username), 1
+    );
+    socket.broadcast.emit("server-send-danhsach-Users",mangUsers);
+  });
+
+  socket.on("user-send-message", function(data){
+    io.sockets.emit("server-send-mesage", {un:socket.Username, nd:data} );
+  });
+
+  socket.on("toi-dang-go-chu", function(){
+    var s = socket.Username + " dang go chu";
+    io.sockets.emit("ai-do-dang-go-chu", s);
+  });
+
+  socket.on("toi-stop-go-chu", function(){
+    io.sockets.emit("ai-do-STOP-go-chu");
+  });
+
+
+});
+*/
+app.get("/", function(req, res){
+  res.render("trangchu");
+});
---
+++ public/xuly.js
@@ -0,0 +1,110 @@
+var socket = io("http://localhost:3000");
+    socket.on("server-send-dki-thatbai", function(){
+      alert("Sai Username (co nguoi da dang ki roi!!!)");
+    });// mghe đang ký thất bại 
+
+    socket.on("server-send-danhsach-Usersa", function(data){
+      $("#list_ten_use").html("");
+
+      data.forEach(function(i){
+        $("#list_ten_use").append("<div class='user'>" + i + "</div>");
+      });
+    });
+
+    socket.on("server-send-danhsach-Usersb", function(data){
+      $("#list_bienso_use").html("");
+
+      data.forEach(function(i){
+        $("#list_bienso_use").append("<div class='user'>" + i + "</div>");
+      });
+    });
+
+
+    socket.on("server-send-dki-thanhcong", function(data){
+     // $("#list_ten_use").html(data);
+    //  $("#list_bienso_use").html(data);
+    $("#login_register").show(1000);
+      $("#loginForm").hide(1000);
+      $("#list_regiter").show(1000); // đăng ký thành công và hiện form danh sách  
+
+
+    });
+
+
+    $(document).ready(function(){
+        $("#hienthi").show();
+        $("#login_register").hide();
+        $("#loginForm").hide();
+        $("#list_regiter").hide();
+        $("#hanhdz").click(function(){
+          $("#login_register").show(1000);
+          $("#loginForm").show(1000);
+          $("#hienthi").hide();
+          $("#list_regiter").hide();
+
+          $("#id_uesregister").click(function(){
+            socket.emit("client-send-aUsername", $("#id_name_register").val()); 
+            socket.emit("client-send-bUsername", $("#id_license_register").val());
+        });
+     $("#btnLogout").click(function(){
+      $("#hienthi").show(1000);
+      $("#login_register").hide();
+      $("#loginForm").hide();
+      $("#list_regiter").hide();
+
+      });
+
+      });  
+
+  });
+
+ /* 
+socket.on("server-send-danhsach-Users", function(data){
+  $("#boxContent").html("");
+  data.forEach(function(i){
+    $("#boxContent").append("<div class='user'>" + i + "</div>");
+  });
+});
+
+
+socket.on("server-send-mesage", function(data){
+  $("#listMessages").append("<div class='ms'>" + data.un + ":" + data.nd +"</div>");
+});
+
+socket.on("ai-do-dang-go-chu", function(data){
+  $("#thongbao").html("<img width='20px' src='typing05.gif'> " + data);
+});
+
+socket.on("ai-do-STOP-go-chu", function(){
+  $("#thongbao").html("");
+});
+
+
+$(document).ready(function(){
+  $("#loginForm").show();
+  $("#chatForm").hide();
+
+  $("#txtMessage").focusin(function(){
+    socket.emit("toi-dang-go-chu");
+  })
+
+  $("#txtMessage").focusout(function(){
+    socket.emit("toi-stop-go-chu");
+  })
+
+  $("#btnRegister").click(function(){
+    socket.emit("client-send-Username", $("#txtUsername").val());
+  });
+
+  $("#btnLogout").click(function(){
+    socket.emit("logout");
+    $("#chatForm").hide(2000);
+    $("#loginForm").show(1000);
+  });
+
+  $("#btnSendMessage").click(function(){
+    socket.emit("user-send-message", $("#txtMessage").val());
+  });
+
+
+});*/
